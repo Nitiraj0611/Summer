bcb8d593f7b2164eb69c37937ef03918c8db61e3
Nitiraj0611 <2020umt1352@mnit.ac.in>
2023-06-23 14:34:00 +0530

    data transformation

---
+++ src/utils.py
@@ -0,0 +1,57 @@
+import os
+import sys
+
+import numpy as np 
+import pandas as pd
+import pickle
+import dill
+
+from sklearn.metrics import r2_score, mean_absolute_error
+
+from src.exception import CustomException
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            dill.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+
+        for model_name, model in models.items():
+            model.fit(X_train, y_train)
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_r2_score = r2_score(y_train, y_train_pred)
+            test_r2_score = r2_score(y_test, y_test_pred)
+            test_mae = mean_absolute_error(y_test, y_test_pred)
+
+            report[model_name] = {
+                'R2 Score (Train)': train_r2_score,
+                'R2 Score (Test)': test_r2_score,
+                'MAE (Test)': test_mae
+            }
+
+        return report
+
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
